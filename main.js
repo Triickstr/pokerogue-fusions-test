7731d5970d6c9a752fe3294f1026673620a4c72b
Triickstr <67447880+Triickstr@users.noreply.github.com>
2025-05-15 12:35:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -283,17 +283,19 @@ allTypes.forEach(attackingType => {
 });
 
 if (ability === "Delta Stream" && fusionTypes.includes("Flying")) {
+    const affectedTypes = ["Rock", "Electric", "Ice"];
     Object.keys(multipliers).forEach(type => {
-        const currentMultiplier = multipliers[type];
-        // Reduce by one tier
-        if (currentMultiplier === 4) {
-            multipliers[type] = 2;
-        } else if (currentMultiplier === 2) {
-            multipliers[type] = 1;
-        } else if (currentMultiplier === 1) {
-            multipliers[type] = 0.5;
-        } else if (currentMultiplier === 0.5) {
-            multipliers[type] = 0.25;
+        if (affectedTypes.includes(type)) {
+            const currentMultiplier = multipliers[type];
+            if (currentMultiplier === 4) {
+                multipliers[type] = 2;
+            } else if (currentMultiplier === 2) {
+                multipliers[type] = 1;
+            } else if (currentMultiplier === 1) {
+                multipliers[type] = 0.5;
+            } else if (currentMultiplier === 0.5) {
+                multipliers[type] = 0.25;
+            }
         }
     });
 }
